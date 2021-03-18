050ee8fea085145f9084e03c4d0aee1f75230afa
adaxelx <adaxelx@o2.pl>
2021-03-18 14:43:18 +0100

    add sceletons of all views

---
+++ src/views/index.js
@@ -0,0 +1,4 @@
+export { default as Dashboard } from './Dashboard';
+export { default as Calendar } from './Calendar';
+export { default as LoginPage } from './LoginPage';
+export { default as RegisterPage } from './RegisterPage';
