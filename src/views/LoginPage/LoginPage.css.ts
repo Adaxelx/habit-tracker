092c8b0218e7ce0e77495dbc1779298ef8395712
adaxelx <adaxelx@o2.pl>
2021-03-31 12:05:49 +0200

    add LoginPage styles

---
+++ src/views/LoginPage/LoginPage.css.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { FlexColCenter } from 'styles';
+
+export const StyledWrapper = styled(FlexColCenter)`
+  height: 100vh;
+  width: 100%;
+`;
--- src/views/LoginPage/LoginPage.tsx
+++ src/views/LoginPage/LoginPage.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Input, Container, Button } from 'components';
+import { StyledWrapper } from './LoginPage.css';
 
-const LoginPage = () => <></>;
+const LoginPage = () => (
+  <Container>
+    <StyledWrapper>
+      <Input label="Login" id="login" />
+      <Input label="Password" id="password" />
+      <Button size="m">Log in</Button>
+    </StyledWrapper>
+  </Container>
+);
 
 export default LoginPage;
