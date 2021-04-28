917bcc9e4b4d3d3143ae118330aa423b861780fc
adaxelx <adaxelx@o2.pl>
2021-04-28 21:55:23 +0200

    add useQuery to CalendarPage

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
 export { default as TestUtil } from './TestUtil';
 export { default as RenderWithRouter } from './RenderWithRouter';
 export * from './validators';
-export * from './types.d';
+export * from './types';
--- src/views/CalendarPage/CalendarPage.tsx
+++ src/views/CalendarPage/CalendarPage.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { Alert } from 'components';
 import { useUserContext } from 'context';
 import { getISODate } from 'constants/calendar';
-import { Event, Label, SIDES } from 'utils';
+import { useQuery } from 'hooks';
+import { Label, SIDES } from 'utils';
+import { Event } from 'utils/types';
 import { StyledCenter } from './CalendarPage.css';
 import { CalendarGrid } from './components';
 import { getEvents, getLabels } from './CalendarPage.api';
@@ -12,32 +15,25 @@ const CalendarPage = () => {
   const [date, setDate] = useState(new Date());
   const [actualMonth, setActualMonth] = useState(date.getMonth());
   const [actualYear, setActualYear] = useState(date.getFullYear());
-  const [events, setEvents] = useState<Event[]>([]);
-  const [labels, setLabels] = useState([]);
+
+  const from = getISODate(new Date(actualYear, actualMonth, 2));
+  const daysInMonth = 33 - new Date(actualYear, actualMonth, 32).getDate();
+  const to = getISODate(new Date(actualYear, actualMonth, daysInMonth));
   const { token } = useUserContext();
+  const [events, setEvents] = useState<Event[]>([]);
+
+  const [eventsF, loadingE, errorE] = useQuery<Event>([from, to, token], () =>
+    getEvents(token, from, to),
+  );
+  const [labels, loadingL, errorL] = useQuery<Label>([token], () => getLabels(token));
 
   useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const from = getISODate(new Date(actualYear, actualMonth, 2));
-        const daysInMonth = 33 - new Date(actualYear, actualMonth, 32).getDate();
-        const to = getISODate(new Date(actualYear, actualMonth, daysInMonth));
-        const responseEvents = await getEvents(token, from, to);
-        const responseLabels = await getLabels(token);
-        const mappedResponse: Event[] = responseEvents.map((event: Event) => {
-          const label = responseLabels.labels.filter(
-            (label: Label) => event?.label === label?._id,
-          )[0];
-
-          return { ...event, label };
-        });
-        setEvents(mappedResponse);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchEvents();
-  }, [actualYear, actualMonth]);
+    const mappedResponse: Event[] = eventsF?.map((event: Event) => {
+      const label = labels?.filter((label: Label) => event?.label === label?._id)[0];
+      return { ...event, label };
+    });
+    setEvents(mappedResponse);
+  }, [eventsF]);
 
   const moveDate = (side: SIDES) => {
     if (side === SIDES.LEFT) {
@@ -60,6 +56,7 @@ const CalendarPage = () => {
   return (
     <StyledCenter>
       <CalendarGrid events={events} month={actualMonth} moveDate={moveDate} year={actualYear} />
+      <Alert loading={loadingE || loadingL} error={errorE || errorL} />
     </StyledCenter>
   );
 };
