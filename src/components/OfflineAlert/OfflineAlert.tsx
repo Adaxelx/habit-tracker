ad6012e311af4298ecd08d860a73592699803b9d
adaxelx <adaxelx@o2.pl>
2021-06-05 22:33:36 +0200

    fix jsx offline alert problem

--- src/components/OfflineAlert/OfflineAlert.tsx
+++ src/components/OfflineAlert/OfflineAlert.tsx
@@ -5,16 +5,13 @@ import { PlainAlert } from '..';
 
 const OfflineAlert = () => {
   const [visible, setVisible] = useState(true);
-  return (
-    !navigator.onLine &&
-    visible && (
-      <StyledWrapper onClick={() => setVisible(false)}>
-        <PlainAlert type={AlertTypes.LOADING}>
-          No internet connnection. Data is loaded from cache.
-        </PlainAlert>
-      </StyledWrapper>
-    )
-  );
+  return !navigator.onLine && visible ? (
+    <StyledWrapper onClick={() => setVisible(false)}>
+      <PlainAlert type={AlertTypes.LOADING}>
+        No internet connnection. Data is loaded from cache.
+      </PlainAlert>
+    </StyledWrapper>
+  ) : null;
 };
 
 export default OfflineAlert;
