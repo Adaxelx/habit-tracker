160de2aef4a32f7b3f09680adc2b609c257015fd
Adrian Mostowski <akmostowski@gmail.com>
2022-05-09 22:20:25 +0200

    add husky, polished and new grays colors

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,5 +1,6 @@
 import core from 'styles/themes/core';
 import { DefaultTheme } from 'styled-components';
+import { hsl } from 'polished';
 
 const lightBlue = '#AFE9FF';
 
@@ -8,6 +9,22 @@ const defaultTheme: DefaultTheme = {
   gridTile: {
     size: 40,
   },
+  newColors: {
+    grays: {
+      50: hsl(210, 19, 10),
+      100: hsl(210, 15, 20),
+      200: hsl(210, 15, 25),
+      300: hsl(210, 10, 40),
+      400: hsl(210, 9, 45),
+      500: hsl(210, 8, 50),
+      600: hsl(210, 12, 50),
+      700: hsl(210, 14, 66),
+      800: hsl(210, 20, 77),
+      900: hsl(210, 25, 88),
+      1000: hsl(210, 25, 96),
+    },
+  },
+  // Old styles to be removed
   colors: {
     ...core.colors,
     body: '#F3FFF2',
